0a37d3a167b27753b8fb630b264e18fe5f844430
Glib Ivashkevych <glib.ivashkevych@gmail.com>
2013-09-15 17:44:43 +0300

    Solution to HW2 added.

--- 2/hw/filescan.py
+++ 2/hw/filescan.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
 import sys
+import os
 
 USAGE_MESSAGE = '''Filename is missing.
 Usage: python filescan.py FILENAME'''
 SUFFIX = "_filtered."
 
-def scan(filein, fileout=None):
+def scan_straightforward(filein, fileout=None):
     '''Main work is done here. This function must read file with filename filein,
     scan it and then output filtered data into file with name fileout.
 
@@ -18,16 +19,95 @@ def scan(filein, fileout=None):
         Filename of the output file. If None, filename for output file
         will be contructed using default suffix.
 
+    Returns
+    -------
+    result : boolean
+        True if filtering was succesful (i.e. filtered file was successfully written), False otherwise
+
     Examples
     --------
-    >>> scan('experiment254.dat')
+    >>> scan_straightforward('experiment254.dat')
 
-    >>> scan('experiment254.dat', 'experiment254_filtered.dat')
+    >>> scan_straightforward('experiment254.dat', 'experiment254_filtered.dat')
     '''
     if not fileout:
     	fileout = SUFFIX.join(filein.rsplit('.',1))
+    #Check if output file already exists
+    if os.path.exists(fileout):
+        print "File %s already exists." % (fileout)
+        return False
 
-    #TODO: Enter your code here
+    try:
+        fin = open(filein, 'r')
+        fout = open(fileout, 'w')
+        #Scaning through input file
+        for line in fin:
+            #Have we 'nan' in this line?
+            if line.find('nan') != -1:
+                continue
+            #Is this a header line?
+            if line.find('#') != -1:
+                fout.write(line)
+                continue
+            #Now we know it's data line. Translating text into numbers
+            numbers = []
+            for strnum in line.split(' ')[1:]:
+                numbers.append(float(strnum))
+            #Check if criterion is satisfied and writing line to output file
+            if sum(numbers) < .5 * len(numbers):
+                fout.write(line)
+        #Closing files
+        fin.close()
+        fout.close()
+        return True
+    except (IOError, ValueError) as e:
+        print e
+        return False
+
+def scan_clean(filein, fileout=None):
+    '''Cleaner implementation of scaning using Python's with statement and list comprehensions.
+
+    Parameters
+    ----------
+    filein : str
+        Filename of file to filter.
+    fileout : str, optional
+        Filename of the output file. If None, filename for output file
+        will be contructed using default suffix.
+
+    Returns
+    -------
+    result : boolean
+        True if filtering was succesful (i.e. filtered file was successfully written), False otherwise
+
+    Examples
+    --------
+    >>> scan_clean('experiment254.dat')
+
+    >>> scan_clean('experiment254.dat', 'experiment254_filtered.dat')
+    '''
+    if not fileout:
+        fileout = SUFFIX.join(filein.rsplit('.',1))
+    if os.path.exists(fileout):
+        print "File %s already exists." % (fileout)
+        return False
+
+    try:
+        with open(filein, 'r') as fin, open(fileout, 'w') as fout:
+            for line in fin:
+                if line.find('#') != -1:
+                    fout.write(line)
+                    continue
+                numbers = [float(strnum) for strnum in line.lower().split(' ')[1:]]
+                if sum(numbers) < .5 * len(numbers):
+                    fout.write(line)
+        return True
+    except (IOError, ValueError) as e:
+        print e
+        return False
+
+def scan(filein, fileout=None):
+    scan_clean(filein, fileout)
 
 if __name__ == "__main__":
     #Checking, whether command line arguments are passed correctly
