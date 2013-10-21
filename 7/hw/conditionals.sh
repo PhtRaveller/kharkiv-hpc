55e888c929e760e62e149b1d3f5d491a25d62df3
Glib Ivashkevych <glib.ivashkevych@gmail.com>
2013-10-22 01:30:23 +0300

    Simple HW on bash if and for added.

---
+++ 7/hw/conditionals.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/sh
+
+# $1..$9 ${10}.. are used a positional arguments
+# $# is the curent number of arguments
+# $@ are all arguments at once. Note difference between $@ and "$@"
+# Try to call this script with some parameters
+
+# Loop over positional arguments
+echo "Using explicit for-loop:"
+for arg in "$@"; {
+    echo $arg
+}
+
+echo "Using explicit for-loop without quoting:"
+for arg in $@; {
+    echo $arg
+}
+
+# Positional arguments are implicitly used in this loop
+echo "Using implicit for-loop:"
+for arg
+do
+    echo $arg
+done
+
+# Note command substitution and test brackets
+echo -e "\nTesting who you are:"
+if [ $(id -u) -eq 0 ]; then
+    echo "You're a root"
+else
+    echo "You're ordinary user with ID $(id -u)"
+fi
+
+echo -e "\nLooking, whether we have git repo here:"
+if git branch &> /dev/null; then
+    echo "We have repo here, branch is $(git branch | sed 's#* ##')"
+else
+    echo "We have no repo here. Strange."
+fi
